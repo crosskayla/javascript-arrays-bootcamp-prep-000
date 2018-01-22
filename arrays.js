252a5fcabdf4d04eff4075966b0d65c96b74d43e
Kayla Cross <kac288@georgetown.edu>
2018-01-22 19:35:42 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,39 @@
+var grilledCheeseIngredients = [
+  "bread",
+  "mild cheese",
+  "sharp cheese",
+  "butter",
+  "tomato",
+  "garlic"
+]
+
+var tomatoSauceIngredients = [
+  "tomato",
+  "garlic",
+  "olive oil",
+  "basil",
+  "oregano"
+]
+
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+]
+
+function addElementToBeginningofArray(array, element){
+  return [element,...array]
+}
+
+function destructivelyAddElementToBeginningofArray(array, element){
+  return array.unshift(element)
+}
+
+function addElementToEndofArray(array, element){
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  return array.push(element)
+}
